3ab8ef76e5437f9b61ca29bb286f8d0233283bda
MD. Maniruzzaman Akash <akash.corp@akij.net>
2020-02-16 03:15:18 +0600

    Updates

---
+++ precache-manifest.9c7025fdc90088d02392fdbcd812bce8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9d903e02e6012dd2ae4349802dc756",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "1fa3c4dd3f0d114b7920",
+    "url": "/react-calculator/static/css/2.3c9e0de5.chunk.css"
+  },
+  {
+    "revision": "613b61c93d3ddc81f54a",
+    "url": "/react-calculator/static/css/main.7739483f.chunk.css"
+  },
+  {
+    "revision": "1fa3c4dd3f0d114b7920",
+    "url": "/react-calculator/static/js/2.91d4492a.chunk.js"
+  },
+  {
+    "revision": "96009c553534be9038b71df35e35452f",
+    "url": "/react-calculator/static/js/2.91d4492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613b61c93d3ddc81f54a",
+    "url": "/react-calculator/static/js/main.80d1529d.chunk.js"
+  },
+  {
+    "revision": "f98e2035d7be80090b09",
+    "url": "/react-calculator/static/js/runtime-main.aa615e56.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-calculator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-calculator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-calculator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-calculator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-calculator/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
